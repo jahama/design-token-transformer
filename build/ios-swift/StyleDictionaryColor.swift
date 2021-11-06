926b1f0b882ccfb635d441992ed0534d12939ff4
Automated Publisher <actions@users.noreply.github.com>
2021-11-06 01:18:24 +0000

    Automated publish: Sat Nov  6 01:18:24 UTC 2021 590e8f32fdbf74fe53e87a13f045f2627f202859

--- build/ios-swift/StyleDictionaryColor.swift
+++ build/ios-swift/StyleDictionaryColor.swift
@@ -4,66 +4,37 @@
 //
 
 // Do not edit directly
-// Generated on Fri, 17 Sep 2021 10:23:13 GMT
+// Generated on Sat, 06 Nov 2021 01:18:23 GMT
 
 
 import UIKit
 
 public enum StyleDictionaryColor {
-    public static let colorsBackground = #ffffffff
-    public static let colorsBackground = #000000ff
-    public static let colorsBackgroundHighlight = #17181aff
-    public static let colorsBackgroundHighlight = #2c2e30ff
-    public static let colorsBackgroundVariant = #f1f2f4ff
-    public static let colorsBackgroundVariant = #222325ff
-    public static let colorsOnAlwaysWhiteHighEmphasis = #020203ff
-    public static let colorsOnAlwaysWhiteHighEmphasis = #020203ff
-    public static let colorsOnAlwaysWhiteLowEmphasis = #717579ff
-    public static let colorsOnAlwaysWhiteLowEmphasis = #717579ff
-    public static let colorsOnAlwaysWhiteMediumEmphasis = #515258ff
-    public static let colorsOnAlwaysWhiteMediumEmphasis = #515258ff
-    public static let colorsOnBackgroundHighEmphasis = #020203ff
-    public static let colorsOnBackgroundHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnBackgroundHighlightLowEmphasis = #787f88ff
-    public static let colorsOnBackgroundHighlightLowEmphasis = #9197a1ff
-    public static let colorsOnBackgroundHighlightMediumEmphasis = #b2b4b9ff
-    public static let colorsOnBackgroundHighlightMediumEmphasis = #afb0b6ff
-    public static let colorsOnBackgroundLowEmphasis = #6a6d71ff
-    public static let colorsOnBackgroundLowEmphasis = #848a95ff
-    public static let colorsOnBackgroundMediumEmphasis = #515258ff
-    public static let colorsOnBackgroundMediumEmphasis = #afb0b6ff
-    public static let colorsOnUiHighEmphasis = #020203ff
-    public static let colorsOnUiHighEmphasis = #f7f8f9ff
-    public static let colorsOnUiHighlightHighEmphasis = #f7f8f9ff
-    public static let colorsOnUiHighlightHighEmphasis = #020203ff
-    public static let colorsOnUiHighlightLowEmphasis = #787f88ff
-    public static let colorsOnUiHighlightLowEmphasis = #717579ff
-    public static let colorsOnUiHighlightMediumEmphasis = #b2b4b9ff
-    public static let colorsOnUiHighlightMediumEmphasis = #515258ff
-    public static let colorsOnUiLowEmphasis = #676a6eff
-    public static let colorsOnUiLowEmphasis = #878991ff
-    public static let colorsOnUiMediumEmphasis = #515258ff
-    public static let colorsOnUiMediumEmphasis = #bfc2c8ff
-    public static let colorsSemanticError = #d4000dff
-    public static let colorsSemanticError = #d4000dff
-    public static let colorsSemanticSuccess = #00af2cff
-    public static let colorsSemanticSuccess = #00af2cff
-    public static let colorsSemanticWarning = #ff8a00ff
-    public static let colorsSemanticWarning = #ff8a00ff
-    public static let colorsSpecialAlwaysDarkOverlay = #000000cc
-    public static let colorsSpecialAlwaysDarkOverlay = #000000cc
-    public static let colorsSpecialAlwaysWhite = #ffffffff
-    public static let colorsSpecialAlwaysWhite = #ffffffff
-    public static let colorsUiHighContrast = #a8aaafff
-    public static let colorsUiHighContrast = #6e7175ff
-    public static let colorsUiHighlight = #17181aff
-    public static let colorsUiHighlight = #fdfeffff
-    public static let colorsUiLowContrast = #f1f2f4ff
-    public static let colorsUiLowContrast = #222325ff
-    public static let colorsUiMediumContrast = #d8daddff
-    public static let colorsUiMediumContrast = #424447ff
-    public static let colorsUiNormalContrast = #c0c2c6ff
-    public static let colorsUiNormalContrast = #4d4f53ff
+    public static let colorsMultipleFills0 = UIColor(red: 0.251, green: 1.000, blue: 0.729, alpha: 1)
+    public static let colorsMultipleFills1 = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 0.1)
+    public static let colorsSingleBlue = UIColor(red: 0.016, green: 0.290, blue: 1.000, alpha: 1)
+    public static let colorsSpecialCharacters = UIColor(red: 0.251, green: 0.875, blue: 0.314, alpha: 1) /* Emoji */
+    public static let colorsSpecialCharactersNderung = UIColor(red: 0.204, green: 0.337, blue: 0.686, alpha: 1)
+    public static let darkBackground = UIColor(red: 0.000, green: 0.000, blue: 0.000, alpha: 1)
+    public static let dashedOutsideStroke = rgba(0, 0, 0, 1)
+    public static let dropShadowSingleColor = rgba(0, 0, 0, 0.25)
+    public static let gradientMultiple0Stops0Color = rgba(255, 184, 0, 1)
+    public static let gradientMultiple0Stops1Color = rgba(255, 184, 0, 1)
+    public static let gradientMultiple1Stops0Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple1Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple2Stops0Color = rgba(207, 48, 48, 1)
+    public static let gradientMultiple2Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientMultiple3Stops0Color = rgba(74, 79, 204, 1)
+    public static let gradientMultiple3Stops1Color = rgba(255, 255, 255, 1)
+    public static let gradientSingleWithMultipleColorStopsStops0Color = rgba(255, 184, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops1Color = rgba(255, 138, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops2Color = rgba(255, 46, 0, 1)
+    public static let gradientSingleWithMultipleColorStopsStops3Color = rgba(255, 0, 0, 1)
+    public static let innerShadowMultiple0Color = rgba(0, 0, 0, 0.25)
+    public static let innerShadowMultiple1Color = rgba(0, 0, 0, 1)
+    public static let innerShadowMultiple2Color = rgba(0, 0, 0, 0.25)
+    public static let lightBackground = UIColor(red: 1.000, green: 1.000, blue: 1.000, alpha: 1)
+    public static let singleStroke = rgba(0, 0, 0, 1)
+    public static let singleStyleStroke = rgba(4, 74, 255, 1)
+    public static let unsupportedMultipleBordersStroke = rgba(255, 230, 0, 1)
 }
